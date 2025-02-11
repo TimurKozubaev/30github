99f240763303648d2332bd6695d68ad1f294b4a6
timur <kozubaevtimur30@gmail.com>
2025-02-11 23:17:53 +0600

    add all file and create model User

---
+++ src/main/java/com/kozubaev/ayu/osago/project/model/User.java
@@ -0,0 +1,20 @@
+package com.kozubaev.ayu.osago.project.model;
+
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+}
---
+++ src/test/java/com/kozubaev/ayu/osago/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kozubaev.ayu.osago.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
