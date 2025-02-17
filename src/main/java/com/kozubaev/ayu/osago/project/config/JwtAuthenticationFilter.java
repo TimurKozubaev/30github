c85745b4698f70532615ea85b8aa6bdf32b3ac21
timur <kozubaevtimur30@gmail.com>
2025-02-17 18:41:48 +0100

    jwt extractUsername

--- src/main/java/com/kozubaev/ayu/osago/project/config/JwtAuthenticationFilter.java
+++ src/main/java/com/kozubaev/ayu/osago/project/config/JwtAuthenticationFilter.java
@@ -13,8 +13,18 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
-
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String jwtToken ;
+        final String username;
+        if(authorizationHeader==null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwtToken=authorizationHeader.substring(7);
+        username=jwtService.extractUsername(jwtToken);
     }
 }
