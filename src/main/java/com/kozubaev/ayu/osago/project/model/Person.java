05c2ae9327b604884f14c74ade0bb259d8aa6b79
12Zholdoshbek <zholdsohbektashiev@gmail.com>
2025-02-23 23:10:36 +0600

    add person

---
+++ src/main/java/com/kozubaev/ayu/osago/project/model/Person.java
@@ -0,0 +1,8 @@
+package com.kozubaev.ayu.osago.project.model;
+
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+}
