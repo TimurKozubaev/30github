8bae2c9808dd9168421eb5ab79791ddbe89e533b
timur <kozubaevtimur30@gmail.com>
2025-02-25 23:33:46 +0600

    create person

--- src/main/java/com/kozubaev/ayu/osago/project/model/Person.java
+++ src/main/java/com/kozubaev/ayu/osago/project/model/Person.java
@@ -1,8 +1,22 @@
 package com.kozubaev.ayu.osago.project.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@Entity
+@Table(name = "person")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
     private String middleName;
+    private LocalDate birthDate;
 }
