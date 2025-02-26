23748c4cc14c9f589e146ca9a96bdbb2a6630c83
timur <kozubaevtimur30@gmail.com>
2025-02-26 17:35:26 +0600

    swagger

--- src/main/java/com/kozubaev/ayu/osago/project/config/OpenApiConfig.java
+++ src/main/java/com/kozubaev/ayu/osago/project/config/OpenApiConfig.java
@@ -1,9 +1,7 @@
 package com.kozubaev.ayu.osago.project.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -14,11 +12,7 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "Auth API",
                 version = "1.0",
-                description = "API для аутентификации пользователей",
-                contact = @Contact(
-                        name = "Your Name",
-                        email = "your.email@example.com"
-                )
+                description = "API для аутентификации пользователей"
         ),
         servers = {
                 @Server(
@@ -28,20 +22,10 @@ import org.springframework.context.annotation.Configuration;
         }
 )
 @SecurityScheme(
-        name = "Bearer Authentication",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer",
-        in = SecuritySchemeIn.HEADER
-)
-
-@SecurityScheme(
         name = "bearerAuth",
-        description = "JWT auth description",
-        scheme = "bearer",
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
-        in = SecuritySchemeIn.HEADER
+        scheme = "bearer"
 )
 public class OpenApiConfig {
 }
