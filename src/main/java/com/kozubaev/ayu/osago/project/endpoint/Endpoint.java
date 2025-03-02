bac35a5616bb43dbadbe4aad8b9ccd743ea73003
timur <kozubaevtimur30@gmail.com>
2025-03-02 23:07:36 +0600

    endpoint

--- src/main/java/com/kozubaev/ayu/osago/project/endpoint/Endpoint.java
+++ src/main/java/com/kozubaev/ayu/osago/project/endpoint/Endpoint.java
@@ -1,4 +1,11 @@
 package com.kozubaev.ayu.osago.project.endpoint;
 
+import com.kozubaev.ayu.osago.project.service.UserService;
+
 public class Endpoint {
+    private final UserService userService;
+
+    public Endpoint(UserService userService) {
+        this.userService = userService;
+    }
 }
